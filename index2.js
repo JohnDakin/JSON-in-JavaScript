9ff2ad4e80d7437681f11d421ec4be78d1c9752f
John Dakin <jdakin749@gmail.com>
2026-07-22 01:33:04 +0300

    Replaced the fetch url

--- index2.js
+++ index2.js
@@ -7,7 +7,7 @@ const output = document.querySelector("#output");
 function addJSON(){
   console.log('clicked');
   output.innerHTML = "WORKING";
-  const url = "https://api.myjson.com/bins/hqys2"; // url must be updated to a working endpoint as myjson.com is no longer available
+  const url = "https://dummyjson.com/users/1";
   fetch(url).then(function(response){
     console.log(response.status);
     return response.json()
@@ -15,7 +15,7 @@ function addJSON(){
   })
   .then(function(data){
     console.log(data);
-    output.innerHTML = "FirstName= " + data.first + "LastName=" + data.last;
+    output.innerHTML = "FirstName= " + data.firstName + " LastName=" + data.lastName;
     // console.log(JSON.parse(data));
   })
 }
